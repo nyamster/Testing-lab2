813bcc252a3ab032a46b8af1b242938e75c1e360
Your Name <you@example.com>
2021-05-27 02:10:52 +0300

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         SystemCalculator systemCalculator = new SystemCalculator();
-        for(double i = -50d; i <= 50d; i += 10d)
+        for(double i = -150d; i <= 150d; i += 10d)
         {
             System.out.println(systemCalculator.calculate(i, 0.00000001));
         }
